3f1e3a090764cb4842b366bd7fb151e904cae1cb
mehulnagar12 <mehulnagar26@gmail.com>
2025-03-08 19:25:54 +0530

    Digital Library project created

---
+++ src/test/java/com/example/digitalLibrary/DigitalLib/DigitalLibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.digitalLibrary.DigitalLib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalLibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
